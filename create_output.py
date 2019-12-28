56aa579b2c5f5d2c1ff3e1912a6a20bd8aedda50
Andrew Haselton <25783@oxfordwildcats.org>
2019-12-27 19:24:54 -0500

    Add creating file and directory if necessary
    Easier for usability

--- create_output.py
+++ create_output.py
@@ -1,4 +1,9 @@
 from jinja2 import Environment, FileSystemLoader
+from os import path, makedirs
+
+directory_name = "outputs/"
+file_name = "report.html"
+file_path = directory_name + file_name
 
 # Content to be published
 content = "Hello, world!"
@@ -16,7 +21,9 @@ def main(report):
     Render a templates and write it to file.
     :return:
     """
-    with open("outputs/report.html", "w") as f:
+    if not path.exists(directory_name):
+        makedirs(directory_name)
+    with open(file_path, "w+") as f:
         f.write(template.render(event_name=report.event_name, event_year=report.event_code[:4], team_infos=report.team_infos, report=report))
 
 
